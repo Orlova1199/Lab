3008cdbedbcb70c57c8320772a001bb4c727c13e
Orlova1199 <43588573+Orlova1199@users.noreply.github.com>
2018-12-16 12:20:02 +0300

    Add files via upload

---
+++ ProgramЛаба3.cs
@@ -0,0 +1,363 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /////////// Фигуры
+    interface IPrint
+    {
+        void Print();
+    }
+    abstract class Figure : IComparable
+    {
+        public string Type { get; protected set; }
+
+        public abstract double Area();
+        public override string ToString()
+        {
+            return this.Type + " площадью " + this.Area().ToString();
+        }
+        public int CompareTo(object obj)
+        {
+            Figure p = (Figure)obj;
+            if (this.Area() < p.Area()) return -1;
+            else if (this.Area() == p.Area()) return 0;
+            else return 1;
+        }
+    }
+    class Rectangle : Figure, IPrint
+    {
+        double height;
+        double width;
+        public Rectangle(double ph, double pw)
+        {
+            height = ph;
+            width = pw;
+            Type = "Прямоугольник";
+        }
+        public override double Area()
+        {
+            double Result = width * height;
+            return Result;
+        }
+        public void Print()
+        {
+            Console.WriteLine(ToString());
+        }
+    }
+    class Square : Rectangle, IPrint
+    {
+        public Square(double size) : base(size, size)
+        {
+            Type = "Квадрат";
+        }
+    }
+    class Circle : Figure, IPrint
+    {
+        double radius;
+        public Circle(double pr)
+        {
+            radius = pr;
+            Type = "Круг";
+        }
+        public override double Area()
+        {
+            double Result = Math.PI * radius * radius;
+            return Result;
+        }
+        public void Print()
+        {
+            Console.WriteLine(ToString());
+        }
+    }
+
+    /////////// Матрица
+    public interface IMatrixCheckEmpty<T>
+    {
+        T getEmptyElement();
+        bool checkEmptyElement(T element);
+    }
+    public class Matrix<T>
+    {
+        Dictionary<string, T> _matrix = new Dictionary<string, T>();
+        int maxX;
+        int maxY;
+        int maxZ;
+        IMatrixCheckEmpty<T> сheckEmpty;
+        public Matrix(int px, int py, int pz, IMatrixCheckEmpty<T> сheckEmptyParam)
+        {
+            this.maxX = px;
+            this.maxY = py;
+            this.maxZ = pz;
+            this.сheckEmpty = сheckEmptyParam;
+        }
+        public T this[int x, int y, int z]
+        {
+            set
+            {
+                CheckBounds(x, y, z);
+                string key = DictKey(x, y, z);
+                this._matrix.Add(key, value);
+            }
+            get
+            {
+                CheckBounds(x, y, z);
+                string key = DictKey(x, y, z);
+                if (this._matrix.ContainsKey(key))
+                {
+                    return this._matrix[key];
+                }
+                else
+                {
+                    return this.сheckEmpty.getEmptyElement();
+                }
+            }
+        }
+        void CheckBounds(int x, int y, int z)
+        {
+            if (x < 0 || x >= this.maxX)
+            {
+                throw new ArgumentOutOfRangeException("x",
+                "x=" + x + " выходит за границы");
+            }
+            if (y < 0 || y >= this.maxY)
+            {
+                throw new ArgumentOutOfRangeException("y",
+                "y=" + y + " выходит за границы");
+            }
+            if (z < 0 || z >= this.maxZ)
+            {
+                throw new ArgumentOutOfRangeException("z",
+                "z=" + z + " выходит за границы");
+            }
+        }
+        string DictKey(int x, int y, int z)
+        {
+            return x.ToString() + "_" + y.ToString() + "_" + z.ToString();
+        }
+        public override string ToString()
+        {
+            StringBuilder b = new StringBuilder();
+            for (int k = 0; k < this.maxZ; k++)
+            {
+                b.Append("z = " + k);
+                b.Append("\n");
+                for (int j = 0; j < this.maxY; j++)
+                {
+                    b.Append("[");
+                    for (int i = 0; i < this.maxX; i++)
+                    {
+                        if (i > 0)
+                        {
+                            b.Append("\t");
+                        }
+                        if (!this.сheckEmpty.checkEmptyElement(this[i, j, k]))
+                        {
+                            b.Append(this[i, j, k].ToString());
+                        }
+                        else
+                        {
+                            b.Append(" - ");
+                        }
+                    }
+                    b.Append("]\n");
+                }
+            }
+            return b.ToString();
+        }
+    }
+    class FigureMatrixCheckEmpty : IMatrixCheckEmpty<Figure>
+    {
+        public Figure getEmptyElement()
+        {
+            return null;
+        }
+        public bool checkEmptyElement(Figure element)
+        {
+            bool Result = false;
+            if (element == null)
+            {
+                Result = true;
+            }
+            return Result;
+        }
+    }
+
+    /////////// Стэк
+    public class SimpleListItem<T>
+    {
+        public T data { get; set; }
+        public SimpleListItem<T> next { get; set; }
+        public SimpleListItem(T param)
+        {
+            this.data = param;
+        }
+    }
+    public class SimpleList<T> : IEnumerable<T> where T : IComparable
+    {
+        protected SimpleListItem<T> first = null;
+        protected SimpleListItem<T> last = null;
+        public int Count
+        {
+            get { return _count; }
+            protected set { _count = value; }
+        }
+        int _count;
+        public void Add(T element)
+        {
+            SimpleListItem<T> newItem = new SimpleListItem<T>(element);
+            this.Count++;
+            if (last == null)
+            {
+                this.first = newItem;
+                this.last = newItem;
+            }
+            else
+            {
+                this.last.next = newItem;
+                this.last = newItem;
+            }
+        }
+        public SimpleListItem<T> GetItem(int number)
+        {
+            if ((number < 0) || (number >= this.Count))
+            {
+                throw new Exception("Выход за границу индекса");
+            }
+            SimpleListItem<T> current = this.first;
+            int i = 0;
+            while (i < number)
+            {
+                current = current.next;
+                i++;
+            }
+            return current;
+        }
+        public T Get(int number)
+        {
+            return GetItem(number).data;
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            SimpleListItem<T> current = this.first;
+            while (current != null)
+            {
+                yield return current.data;
+                current = current.next;
+            }
+        }
+        System.Collections.IEnumerator
+        System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        public void Sort()
+        {
+            Sort(0, this.Count - 1);
+        }
+        private void Sort(int low, int high)
+        {
+            int i = low;
+            int j = high;
+            T x = Get((low + high) / 2);
+            do
+            {
+                while (Get(i).CompareTo(x) < 0) ++i;
+                while (Get(j).CompareTo(x) > 0) --j;
+                if (i <= j)
+                {
+                    Swap(i, j);
+                    i++; j--;
+                }
+            } while (i <= j);
+            if (low < j) Sort(low, j);
+            if (i < high) Sort(i, high);
+        }
+        private void Swap(int i, int j)
+        {
+            SimpleListItem<T> ci = GetItem(i);
+            SimpleListItem<T> cj = GetItem(j);
+            T temp = ci.data;
+            ci.data = cj.data;
+            cj.data = temp;
+        }
+    }
+    class SimpleStack<T> : SimpleList<T> where T : IComparable
+    {
+        public void Push(T element)
+        {
+            Add(element);
+        }
+        public T Pop()
+        {
+            T Result = default(T);
+            if (this.Count == 0) return Result;
+            if (this.Count == 1)
+            {
+                Result = this.first.data;
+                this.first = null;
+                this.last = null;
+            }
+            else
+            {
+                SimpleListItem<T> newLast = this.GetItem(this.Count - 2);
+                Result = newLast.next.data;
+                this.last = newLast;
+                newLast.next = null;
+            }
+            this.Count--;
+            return Result;
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle(3, 5);
+            Square square = new Square(6);
+            Circle circle = new Circle(2);
+
+            List<Figure> fl = new List<Figure>();
+            fl.Add(circle);
+            fl.Add(rect);
+            fl.Add(square);
+            Console.WriteLine("Сортировка List<Figure>:");
+            fl.Sort();
+            foreach (var x in fl) Console.WriteLine(x);
+
+            ArrayList li = new ArrayList();
+            li.Add(circle);
+            li.Add(rect);
+            li.Add(square);
+            Console.WriteLine("\nСортировка ListArray:");
+            li.Sort();
+            foreach (var x in li) Console.WriteLine(x);
+
+            Console.WriteLine("\nМатрица:");
+            Matrix<Figure> matrix = new Matrix<Figure>(3, 3, 3, new FigureMatrixCheckEmpty());
+            matrix[0, 0, 0] = rect;
+            matrix[1, 1, 1] = square;
+            matrix[2, 2, 2] = circle;
+            Console.WriteLine(matrix.ToString());
+            Console.WriteLine("\nСтэк:");
+            SimpleStack<Figure> stack = new SimpleStack<Figure>();
+            stack.Push(rect);
+            stack.Push(square);
+            stack.Push(circle);
+            while (stack.Count > 0)
+            {
+                Figure f = stack.Pop();
+                Console.WriteLine(f);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
